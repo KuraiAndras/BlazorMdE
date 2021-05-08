761753ee3845fd7d356fb206e129bc0887fc2570
Kurai András <k.andras9642@gmail.com>
2021-05-08 20:15:07 +0200

    implement makrdown View

---
+++ BlazorMdE/Injector.cs
@@ -0,0 +1,22 @@
+﻿using Ganss.XSS;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlazorMdE
+{
+    public static class Injector
+    {
+        public static IServiceCollection AddBlazorMdE(this IServiceCollection services, HtmlSanitizer? sanitizer = null)
+        {
+            services.AddTransient<IHtmlSanitizer, HtmlSanitizer>(_ =>
+            {
+                if (sanitizer is not null) return sanitizer;
+
+                sanitizer = new HtmlSanitizer();
+                sanitizer.AllowedAttributes.Add("class");
+                return sanitizer;
+            });
+
+            return services;
+        }
+    }
+}
---
+++ BlazorMdE/MarkdownView.razor.cs
@@ -0,0 +1,40 @@
+﻿using Ganss.XSS;
+using Markdig;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorMdE
+{
+    public partial class MarkdownView
+    {
+        private string _content = string.Empty;
+
+        [Inject] public IHtmlSanitizer HtmlSanitizer { get; init; } = default!;
+
+        [Parameter]
+        public string Content
+        {
+            get => _content;
+            set
+            {
+                _content = value;
+                HtmlContent = ConvertStringToMarkupString(_content);
+            }
+        }
+
+        [Parameter]
+        public string? Class { get; set; }
+
+        public MarkupString HtmlContent { get; private set; }
+
+        private MarkupString ConvertStringToMarkupString(string value)
+        {
+            if (string.IsNullOrWhiteSpace(_content)) return new MarkupString();
+
+            var html = Markdown.ToHtml(value, new MarkdownPipelineBuilder().UseAdvancedExtensions().Build());
+
+            var sanitizedHtml = HtmlSanitizer.Sanitize(html);
+
+            return new MarkupString(sanitizedHtml);
+        }
+    }
+}
