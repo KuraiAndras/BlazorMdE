7572489fa61406335facf1cb1c3df7673bbccf53
Kurai András <k.andras9642@gmail.com>
2021-05-08 20:45:07 +0200

    add basic editor with preview

---
+++ BlazorMdE/MarkdownEditor.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorMdE
+{
+    public partial class MarkdownEditor
+    {
+        private bool IsEditing { get; set; } = true;
+        [Parameter] public string Content { get; set; } = string.Empty;
+
+        private void SwitchEditView() => IsEditing = !IsEditing;
+    }
+}
