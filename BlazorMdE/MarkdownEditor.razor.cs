78334dc6c6c8a7ccc7bb11ce9f71282f96b91a6a
Kurai András <k.andras9642@gmail.com>
2021-05-08 22:23:03 +0200

    add buttons

--- BlazorMdE/MarkdownEditor.razor.cs
+++ BlazorMdE/MarkdownEditor.razor.cs
@@ -4,9 +4,64 @@ namespace BlazorMdE
 {
     public partial class MarkdownEditor
     {
-        private bool IsEditing { get; set; } = true;
+        private bool ShowPreview { get; set; } = true;
         [Parameter] public string Content { get; set; } = string.Empty;
 
-        private void SwitchEditView() => IsEditing = !IsEditing;
+        private void BoldClicked()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void ItalicClicked()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void HeadingClicked()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void QuoteClicked()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void GenericListClicked()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void NumberedListClicked()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void LinkClicked()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void ImageClicked()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void PreviewClicked() => ShowPreview = !ShowPreview;
+
+        private void SideBySideClicked()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void FullScreenClicked()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void HelpClicked()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
